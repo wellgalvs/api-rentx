2f9a112ec1ee09481765b38e2dbb0cabdb1adaa1
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-09-02 14:53:54 -0300

    Rate Limiter

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import * as redis from "redis";
+
+import { AppError } from "@shared/errors/AppError";
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  const redisClient = redis.createClient({
+    // legacyMode: true,
+    // socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    // }
+  });
+
+  // await redisClient.connect();
+
+  const limiter = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: "rateLimiter",
+    points: 5,
+    duration: 5
+  });
+
+  try {
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError("Too many requests", 429);
+  }
+}
